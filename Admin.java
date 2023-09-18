fa83917fb771d77eac626c10f05f2dfe07d5fc29
JenisonSchmitt <75100716+JenisonSchmitt@users.noreply.github.com>
2023-09-18 15:32:53 -0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,29 +1,32 @@
+package A3;
+
+import javax.swing.JTextField;
+
+
 public class Admin extends Cliente {
 
-    private String numRegistro;
+	private JTextField numregistro;
+	private JTextField nome;
 
-    Admin (String numRegistro) {
-
-        this.numRegistro = numRegistro;
-
+    Admin(){
     }
+    Admin(JTextField nome, JTextField numregistro) {
+		super();
+		this.numregistro = numregistro;
+		this.nome= nome;
+	}
 
-    public String getNumRegistro {
-
-        return numRegistro;
-
-    }
-
-    public void setNumRegistro {
-        this.numRegistro=numRegistro;
-    }
-
-    public Cliente[] getListaCliente (){
-        
-        // metódo para listar
-        // os clientes da petshop
-
-    }
-
-
+    public JTextField getnumregistro() {
+		return numregistro;
+	}
+	public JTextField setnumregistro(JTextField numregistro) {
+		return this.numregistro = numregistro;
+	}
+	public JTextField getnome() {
+		return nome;
+	}
+	public JTextField setnome(JTextField nome) {
+		return this.nome = nome;
+	}
 }
+
